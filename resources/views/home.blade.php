b44bfa787af4f47057915bdc8a7c6f36676c361a
Lucas Jara <lucas.jara@hofmann.cl>
2021-03-01 23:22:55 -0300

    Colaboraciones
    Integracion Gestion/Colaboraciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,11 @@
                 font-size: 3.5rem;
             }
         }
+
+        .card-text,
+        .card-title {
+            color: black;
+        }
     </style>
 </head>
 
@@ -48,9 +53,8 @@
                 <p class="title is-primary">Elige tu demostración</p>
                 <p>Saludos a continuación se encuentra una lista de distintas herramientas que he usado en distintos proyectos con ejemplos prácticos.</p>
                 <div class="btn-group">
-                    <button type="button" class="nes-btn is-primary" data-bs-toggle="modal" data-bs-target="#modalWeb">Sitios Web</button>
-                    <button type="button" class="nes-btn">Mapas</button>
-                    <button type="button" class="nes-btn is-success">Movil</button>
+                    <button type="button" class="nes-btn is-success" data-bs-toggle="modal" data-bs-target="#modalMovil">Proyectos Movil</button>
+                    <button type="button" class="nes-btn is-primary" data-bs-toggle="modal" data-bs-target="#modalWeb">Gestión/Colaboraciones</button>
                 </div>
             </section>
         </main>
@@ -62,12 +66,12 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <link href="https://unpkg.com/nes.css@2.2.0/css/nes.min.css" rel="stylesheet" />
-    <!-- Modales -->
+    <!-- Inicio Modal Web -->
     <div class="modal" tabindex="-1" id="modalWeb">
         <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-primary">
-                    <h5 class="modal-title">Seleccione Sitio</h5>
+                    <h5 class="modal-title">Seleccione para Visitar</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -76,23 +80,23 @@
                             <div class="card mb-4 box-shadow">
                                 <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/CapturaInfest.PNG') }}" data-holder-rendered="true">
                                 <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text">Infest es un evento que busca darle a sus asistentes una experiencia única y enriquecedora en la ciudad de Temuco, todo esto a manos de los videojuegos.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="card mb-4 box-shadow">
-                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177eb7ba1bc%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177eb7ba1bc%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.71875%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                                <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/CapturaRocketLab.PNG') }}" data-holder-rendered="true">
                                 <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text">Somos The RocketLab, productora de contenido offline y online con una basta experiencia desarrollando distintos tipos de actividades a lo largo del país.</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-4">
                             <div class="card mb-4 box-shadow">
-                                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_177eb7ba1bc%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_177eb7ba1bc%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.71875%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                                <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/CapturaSmartride.PNG') }}" data-holder-rendered="true">
                                 <div class="card-body">
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text">Somos una empresa joven , dinámica y que da solución a la venta de Scooter y a la reparación de estos , pero antes que todo, somos Riders, amamos la velocidad.</p>
                                 </div>
                             </div>
                         </div>
@@ -101,7 +105,40 @@
             </div>
         </div>
     </div>
-    <!-- Fin Modal -->
+    <!-- Fin Modal Web -->
+    <!-- Inicio Modal Movil -->
+    <div class="modal" tabindex="-1" id="modalMovil">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-primary">
+                    <h5 class="modal-title">Seleccione para Visitar</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class=row>
+                        <div class="col-6">
+                            <div class="card mb-4 box-shadow">
+                                <h5 class="card-title">PokePixelArt</h5>
+                                <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/CapturaInfest.PNG') }}" data-holder-rendered="true">
+                                <div class="card-body">
+                                    <p class="card-text">Infest es un evento que busca darle a sus asistentes una experiencia única y enriquecedora en la ciudad de Temuco, todo esto a manos de los videojuegos.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="{{ asset('img/CapturaSmartride.PNG') }}" data-holder-rendered="true">
+                                <div class="card-body">
+                                    <p class="card-text">Somos una empresa joven , dinámica y que da solución a la venta de Scooter y a la reparación de estos , pero antes que todo, somos Riders, amamos la velocidad.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Modal Movil -->
 </body>
 
 </html>
